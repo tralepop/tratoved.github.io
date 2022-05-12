839ba988ecacdf2ccc0298e81e6ae4d9d13d0fa7
tralepop <100528033+tralepop@users.noreply.github.com>
2022-05-12 19:29:24 +0300

    Add files via upload

---
+++ wermer1/js/app.js
@@ -0,0 +1,24 @@
+$(function() {
+
+    var header = $("#header"),
+        failH = 0,
+        scrollOffset = 0;
+
+
+    /* Smooth scroll */
+    $("[data-scroll]").on("click", function(event) {
+        event.preventDefault();
+
+        var $this = $(this),
+            blockId = $this.data('scroll'),
+            blockOffset = $(blockId).offset().top;
+
+        $("#nav a").removeClass("active");
+        $this.addClass("active");
+
+        $("html, body").animate({
+            scrollTop:  blockOffset
+        }, 900);
+    });
+
+});
